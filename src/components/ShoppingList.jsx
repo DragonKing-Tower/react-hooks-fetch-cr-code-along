5aa2259f013ace6a71bbe35dec58849048d45d3a
Cyril Rafalovich <replanttrees@gmail.com>
2025-06-09 15:15:18 -1000

    none

--- src/components/ShoppingList.jsx
+++ src/components/ShoppingList.jsx
@@ -1,36 +1,53 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
 
 function ShoppingList() {
-  const [selectedCategory, setSelectedCategory] = useState("All");
-  const [items, setItems] = useState([]);
+	const [selectedCategory, setSelectedCategory] = useState("All");
+	const [items, setItems] = useState([]);
 
-  function handleCategoryChange(category) {
-    setSelectedCategory(category);
+	function handleCategoryChange(category) {
+		setSelectedCategory(category);
+	}
+
+	const itemsToDisplay = items.filter((item) => {
+		if (selectedCategory === "All") return true;
+
+		return item.category === selectedCategory;
+	});
+
+  function handleAddItem(item){
+    setItems((prevItems)=>[...prevItems,item])
   }
 
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+	useEffect(() => {
+		async function fetchItems() {
+			try {
+				const data = await fetch("http://localhost:4000/items");
+				const itemArray = await data.json();
+				setItems(itemArray);
+			} catch (error) {
+				console.log(`Error: ${error}`);
+			}
+		}
+		fetchItems();
+	}, []);
 
-    return item.category === selectedCategory;
-  });
-
-  return (
-    <div className="ShoppingList">
-      <ItemForm />
-      <Filter
-        category={selectedCategory}
-        onCategoryChange={handleCategoryChange}
-      />
-      <ul className="Items">
-        {itemsToDisplay.map((item) => (
-          <Item key={item.id} item={item} />
-        ))}
-      </ul>
-    </div>
-  );
+	return (
+		<div className="ShoppingList">
+			<ItemForm handleAddItem={handleAddItem}/>
+			<Filter
+				category={selectedCategory}
+				onCategoryChange={handleCategoryChange}
+			/>
+			<ul className="Items">
+				{itemsToDisplay.map((item) => (
+					<Item key={item.id} item={item} />
+				))}
+			</ul>
+		</div>
+	);
 }
 
 export default ShoppingList;
